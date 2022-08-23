4f05651721a732a49d68c3bbc9f28521e50bae78
Geryer86 <german.af86@gmail.com>
2022-08-23 17:19:27 -0300

    donde

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useEffect, useRef, useState} from 'react';
 import './App.css';
+import List from './components/List';
+import Form from './components/Form';
+import {Sub, SubsResponseFromApi} from './types';
+
+interface AppState {
+  subs: Array<Sub>,
+  newSubsNumber: number
+}
+
 
 function App() {
+  
+  const [subs, setSubs] = useState<AppState['subs']>([])  // ----> useState<Sub[]>
+  
+  const [newSubsNumber, setNewSubsNumber] = useState<AppState['newSubsNumber']>(0)
+
+  const divRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const fetchSubs = (): Promise<SubsResponseFromApi> => {
+      return fetch('http://localhost:3001/subs').then(res => res.json())
+
+    }
+
+    const mapFromApiToSubs = (apiResponse: SubsResponseFromApi): Array<Sub> => {
+      return apiResponse.map(subFromApi => {
+        const { nick, months: subMonths, profileUrl: avatar, description } = subFromApi
+        return { nick, subMonths, avatar, description }
+      })
+    }
+
+    fetchSubs()
+    .then(mapFromApiToSubs)
+    .then(setSubs)
+  }, [])
+
+  const handleNewSub = (newSub: Sub): void => {
+    setSubs(subs => [...subs, newSub])
+    setNewSubsNumber(n => n + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={divRef}>
+      <h1>Subscriptors List</h1>
+      <List subs={subs} />
+      New Subs: {newSubsNumber}
+      <Form onNewSub={handleNewSub}/>
     </div>
   );
 }
